16fe1b0a39f495cfad7fa3a324c415b03131d878
narakon2543 <narakon.ninr@bumail.net>
2025-12-03 16:44:08 +0700

    Revert "update db name"
    This reverts commit d377bd1738527b5c69abe5c495a0680761f259c2.

--- connect.py
+++ connect.py
@@ -3,7 +3,7 @@ import os
 
 def db_connection():
     base_dir = os.path.dirname(os.path.abspath(__file__))
-    db_path = os.path.join(base_dir, "database", "DB_Book.db")
+    db_path = os.path.join(base_dir, "database", "Be_Lune.db")
 
     conn = sqlite3.connect(db_path)
     cursor = conn.cursor()
